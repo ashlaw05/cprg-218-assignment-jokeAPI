f7bf242f708d3c4bce21911bbd3cfcb27a0f5baf
ashlaw05 <ashley.lawless@edu.sait.ca>
2025-03-21 17:52:38 -0600

    programming
    programming joke was connected to button

--- joke.js
+++ joke.js
@@ -5,4 +5,12 @@ fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
 		joke.textContent = data.joke || data.setup + data.delivery
 	});
 
+	document.getElementById("programming").addEventListener ('click',function() {
+		fetch("https://v2.jokeapi.dev/joke/programming?safe-mode")
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+			joke.textContent = data.joke || data.setup + data.delivery
+		});
+	});
     
